d3e455a0323d85d8e02a7ccfa3605d3df6e8f2d9
marie <mariegacheri@gmail.com>
2023-11-22 10:24:53 +0300

    commit

---
+++ project/src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from './Footer'; 
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <h1>Sales App</h1>
+      <div className="search-bar">
+        <input type="text" placeholder="Search..." />
+        <button type="button">Search</button>
+      </div>
+      <Footer /> {/* Include the Footer component */}
+    </div>
+  );
+};
+
+export default NavBar;
