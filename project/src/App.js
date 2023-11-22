60acba53d1604530fe200a06b47a13a3e19e47e5
2559055 <mohamed.hussein@student.moringaschool.com>
2023-11-22 00:53:26 -0800

    connecting the  components

--- project/src/App.js
+++ project/src/App.js
@@ -1,3 +1,35 @@
 
 
 
+import React, { useState, useEffect } from 'react';
+import NavBar from './components/NavBar';
+import ProductList from './components/ProductListing'; // Assuming your component is named ProductList
+import Footer from './/components/Footer';
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error('Error fetching product data:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []); // Empty dependency array means this effect runs once, like componentDidMount
+
+  return (
+    <div>
+      <NavBar />
+      <ProductList products={products} />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
