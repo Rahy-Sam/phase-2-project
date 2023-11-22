64f9db3862c62a7c73a7c3c38d98f04243dd03e2
MainaZaquir <mzaquir58@gmail.com>
2023-11-22 12:19:10 +0300

    Added a footer

---
+++ project/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                2023 E-Commerce App. All rights reserved.
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
