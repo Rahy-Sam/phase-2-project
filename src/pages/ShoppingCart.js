b1baca3fb8ef1a584f106472cb85618ec00fa7af
MainaZaquir <mzaquir58@gmail.com>
2023-11-24 13:02:29 +0300

    Updated shopping cart

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useCart } from './CartContext';
-import { Card, Button } from 'semantic-ui-react';
+import { useCart } from '../components/CartContext';
+import { Card, Button, Header, Icon } from 'semantic-ui-react';
 
 const ShoppingCart = () => {
   const { cart, removeFromCart } = useCart();
@@ -10,35 +10,41 @@ const ShoppingCart = () => {
   };
 
   const calculateTotal = () => {
-    return cart.reduce((total, item) => total + item.price * item.quantity, 0);
+    return cart.reduce((total, item) => {
+      const itemTotal = item.price * (item.quantity || 1);
+      return total + itemTotal;
+    }, 0);
   };
 
   return (
-    <div>
-      <h2>Shopping Cart</h2>
+    <div style={{ padding: '20px' }}>
+      <Header as="h2">Shopping Cart</Header>
       {cart.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
         <>
           <Card.Group>
             {cart.map((item) => (
-              <Card key={item.id}>
+              <Card key={item.id} fluid>
                 <Card.Content>
                   <Card.Header>{item.title}</Card.Header>
                   <Card.Meta>Quantity: {item.quantity}</Card.Meta>
                   <Card.Description>
-                    Price: ${item.price} (Total: ${item.price * item.quantity})
+                    Price: ${item.price} (Total: ${item.price * (item.quantity || 1)})
                   </Card.Description>
                 </Card.Content>
                 <Card.Content extra>
                   <Button color="red" onClick={() => handleRemoveClick(item.id)}>
+                    <Icon name="trash alternate outline" />
                     Remove
                   </Button>
                 </Card.Content>
               </Card>
             ))}
           </Card.Group>
-          <p>Total: ${calculateTotal()}</p>
+          <Header as="h4" style={{ marginTop: '20px' }}>
+            Total: ${calculateTotal().toFixed(2)}
+          </Header>
         </>
       )}
     </div>
@@ -46,6 +52,3 @@ const ShoppingCart = () => {
 };
 
 export default ShoppingCart;
-
-
-
